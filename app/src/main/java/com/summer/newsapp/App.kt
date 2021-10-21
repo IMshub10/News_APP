11f4be71eeb5ae18ec505152e735843624091849
shubh180199 <54928767+shubh180199@users.noreply.github.com>
2021-10-21 19:32:20 +0530

    basic UI elements added

---
+++ app/src/main/java/com/summer/newsapp/App.kt
@@ -0,0 +1,4 @@
+package com.summer.newsapp
+
+class App {
+}
---
+++ app/src/main/java/com/summer/newsapp/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.summer.newsapp.utils
+
+object Constants {
+    const val NEWS_APP_API_KEY = "8e415d09f29348bb8cddddef7f528812"
+}
